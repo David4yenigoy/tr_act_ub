aa27902e921a25924659f28a0039236037c2afb2
David4yenigoy <94457982+David4yenigoy@users.noreply.github.com>
2021-11-30 18:19:59 +0900

    Update hee_sell.py

--- hee_sell.py
+++ hee_sell.py
@@ -42,7 +42,7 @@ def get_start_time(ticker):
     
 while(True):
     try :
-        now = datetime.datetime.now()
+        now = datetime.datetime.now() + datetime.timedelta(hours=9)
         start_time = get_start_time('KRW-BTC')
         end_time = start_time + datetime.timedelta(days=1)
 
