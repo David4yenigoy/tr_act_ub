a667bd37e8302e5786623ccf58a762fc9c53484b
David4yenigoy <94457982+David4yenigoy@users.noreply.github.com>
2021-12-02 10:15:51 +0900

    Update and rename 3_RSI_trade_act.py to 3_RSI_trade_buy.py

--- 3_RSI_trade_buy.py
+++ 3_RSI_trade_buy.py
@@ -3,11 +3,8 @@ import pandas
 import datetime 
 import time
 
-f = open("./phmemo.txt")
-lines = f.readlines()
-access = lines[1].strip()   # access key
-secret = lines[3].strip()   # secret key
-f.close()
+access = ''
+secret = ''
 
 upbit = pyupbit.Upbit(access, secret)
 
@@ -28,21 +25,18 @@ def rsi(ohlc: pandas.DataFrame, period: int = 14):
 # coinlist = ["KRW-BTC", "KRW-XRP", "KRW-ETC", "KRW-ETH", "KRW-POWR", "KRW-CRO", "KRW-VET", "KRW-AQT", "KRW-AXS", "KRW-EOS", "KRW-BORA", "KRW-PLA", "KRW-WAXP", "KRW-MANA", "KRW-SAND", "KRW-QKC", "KRW-HIVE", "KRW-HUNT", "KRW-DOGE", "KRW-CHZ", "KRW-ADA", "KRW-MOC", "KRW-DOT"] # Coin ticker 추가 
 
 coinlist = pyupbit.get_tickers(fiat="KRW")
-lower28 = []
-higher70 = []
+coinlist.remove("KRW-BTC")
+coinlist.remove("KRW-ETH")
+coinlist.remove("KRW-HIVE")
+coinlist.remove("KRW-CRO")
 
 # 시장가 매수 함수 
 def buy(coin): 
     money = upbit.get_balance("KRW")
     print(coin, datetime.datetime.now(),"Buy_G")
     if money > 21000 : 
-        res = upbit.buy_market_order(coin, 20000) 
-    # elif money < 100000: 
-    #     res = upbit.buy_market_order(coin, money*0.9) 
+        res = upbit.buy_market_order(coin, 20000)     
     return
-
-        # ret = upbit.buy_limit_order("KRW-XRP", 100, 20)
-        # ret = upbit.sell_limit_order("KRW-XRP", 1000, 20)
 
 # 시장가 매도 함수 
 def sell(coin): 
@@ -56,9 +50,12 @@ def sell(coin):
 
 
 # initiate
+lower28 = []
+higher70 = []
+
 for i in range(len(coinlist)):
     lower28.append(False)
-    
+    higher70.append(False)    
 
 while(True):
     for i in range(len(coinlist)):
@@ -67,20 +64,21 @@ while(True):
             now_rsi = rsi(data, 14).iloc[-1]
             amount = upbit.get_balance(coinlist[i]) 
             av_buy = float(upbit.get_avg_buy_price(coinlist[i]))
-            profit_price = round(av_buy*1.02,4)   
+            profit_price = round(av_buy*1.015,4)   
             cur_price = pyupbit.get_current_price(coinlist[i])
             total = amount * cur_price 
             # print(coinlist[i], "현재시간: ", datetime.datetime.now(), "< RSI > :", now_rsi)            
         
             if now_rsi <= 28 :
                 lower28[i] = True
-            elif now_rsi >= 30 and lower28[i] == True and total < 95000 :
+            elif now_rsi >= 30 and lower28[i] == True and higher70[i] == False :
                 buy(coinlist[i])
-                lower28[i] = False
-            elif cur_price >= profit_price and av_buy > 0 :
-                sell(coinlist[i])                
+                higher70[i] = True
+            
             elif now_rsi >= 50 :
                 lower28[i] = False
+                higher70[i] = False
+                
             time.sleep(0.2)
             
         except Exception as e:
